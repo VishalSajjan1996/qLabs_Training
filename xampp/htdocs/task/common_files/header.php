044bb7171b1bddd1aeb89ad4cd65e8a685542a36
VishalSajjan1996 <117075426+VishalSajjan1996@users.noreply.github.com>
2022-12-15 10:52:13 +0530

    Add files via upload

--- xampp/htdocs/task/common_files/header.php
+++ xampp/htdocs/task/common_files/header.php
@@ -1,1 +1,12 @@
-
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Form </title>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/milligram/1.4.1/milligram.css">
+    <link rel="stylesheet" href="assets/css/style.css">
+</head>
+<body>
+    
