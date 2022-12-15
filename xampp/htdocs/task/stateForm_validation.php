ab5dbc5cf978f06e377875c436b465d2a32b5eb2
VishalSajjan1996 <117075426+VishalSajjan1996@users.noreply.github.com>
2022-12-15 10:52:54 +0530

    Add files via upload

---
+++ xampp/htdocs/task/stateForm_validation.php
@@ -0,0 +1,11 @@
+<?php
+$states = $_POST["state"];
+$boolean = $_POST["radio"];
+$numbers = $_POST["number"];
+
+if($numbes )
+
+echo "<p>State : <strong>$states</strong></p>";
+echo "<p>From Karnataka : <strong>$boolean</strong></p>";
+echo "<p>Phone Number : <strong>$numbers</strong></p>";
+?>
